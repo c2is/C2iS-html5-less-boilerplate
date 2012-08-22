b48ed9b3588249da10581b697e4764e44293b12e
David Jomain <kaynshinlogin@gmail.com>
2012-08-22 15:04:42 +0200

    first commit

---
+++ html/includes/inc_header.php
@@ -0,0 +1,21 @@
+<?php include('functions.php');?>
+<!doctype html>
+<html class="js-false" lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title><?php echo $title ?></title>
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="stylesheet" href="../css/screen.css">
+
+    <!--<script src="js/libs/head.js"></script>-->
+    <script src="../js/libs/head.extended.js"></script><!--Version etendue de head.js avec + d options : syntaxe legerement differente -->
+    <script>var templatePath = '../';</script><!-- templatePath : chemin du template en absolue -->
+</head>
+<body>
---
+++ js/front.js
@@ -0,0 +1,126 @@
+/* Project: NOMPROJET - Date: 20120806 - Author: C2iS.fr > TRI */
+
+$(function() { //domReady
+    //addClassGtIe(); // Test msie version and add class .gt-ieX x n // ne pas utiliser si utilisation de head.extended.js
+
+    // ScrollTop onload (mobile) si il n'y a pas d'ancre
+    if(/mobile/i.test(navigator.userAgent) && !location.hash){
+        $(window).scrollTo(0, 1);
+    }
+
+// Test log exe front.js
+   consoleLog('Execution front.js : ok');
+
+// Test html5 form capacties andif do polyfills
+    if(!Modernizr.input.placeholder){ //ie. placeholder
+        polyfillPlaceholder();
+	}
+
+// Gestion du click sur le parent
+    if( $('.linkParent').length > 0 ) {
+        addLinkBlock();
+	}
+
+});
+
+
+
+
+// Gestion du console.log (évite le bug sur ie si la console n'est pas ouverte)
+function consoleLog (data) {
+    if(window.console && console.log )
+        console.log(data);
+}
+
+
+function polyfillPlaceholder(){
+    var active = document.activeElement;
+    $('[type="text"], [type="mail"]').focus(function () {
+        if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
+            $(this).val('').removeClass('hasPlaceholder');
+        }
+    }).blur(function () {
+            if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
+                $(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
+            }
+        });
+    $('[type="text"], [type="mail"]').blur();
+    $(active).focus();
+    $('form:eq(0)').submit(function () {
+        $('[type="text"].hasPlaceholder, [type="mail"].hasPlaceholder').val('');
+    });
+}
+
+
+/* // ne pas utiliser si utilisation de head.extended.js
+// Test msie version and add class .gt-ieX x n
+function addClassGtIe(){
+    if($.browser.msie){
+        var iVersion = parseInt($.browser.version),
+            iLowestVersion = 7; // version la plus basse prise en compte pour l'inté
+        for (i=iLowestVersion;i<iVersion+1;i++){// la version courante est supérieur aux versions précédentes
+            $('html').addClass('gt-ie'+(i-1));
+        }
+    }
+}*/
+
+
+// Gestion du click sur le parent
+function addLinkBlock(){
+    $('.linkParent').each( function( ) {
+        var oElem = $(this).find('.linkBlock'),
+            sOnClick = oElem.attr('onclick');
+        if(sOnClick)
+            $(this).attr('onclick',sOnClick);
+        oElem.removeAttr('onclick');
+
+        $(this).css({cursor:'pointer'}).click(function(e) {
+			var event = e;
+			if (!e)
+				event = window.event;
+			if (event && event.target != oElem[0]) {
+                var sHref = oElem.attr('href'),
+                    sTarget = oElem.attr('target')?oElem.attr('target'):'_self';
+                //consoleLog(sHref);
+                switch (sTarget) {
+                    case "_blank":
+                        window.open(sHref, '');
+                        break;
+                    case "_parent":
+                        parent.location.href = sHref;
+                        break;
+                    case "_top":
+                        top.location.href = sHref;
+                        break;
+                    case "_self":
+                        document.location.href = sHref;
+                        break;
+                    default:
+                        sTarget.location.href = sHref;
+                        break;
+                }
+			}
+		});
+
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
