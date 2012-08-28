48e5a25244df9895369266c1d6fb28b1e5ead919
Eric Fressange <e.fressangedubost@c2is.fr>
2012-08-28 16:25:42 +0200

    [JS] deplacement des fonctions addClassGtIe et addLinkBlock dans plugin.js

--- js/plugins.js
+++ js/plugins.js
@@ -32,3 +32,57 @@ function polyfillPlaceholder(){
     });
     $(active).focus();
 }
+
+
+/* // ne pas utiliser si utilisation de head.extended.js
+ // Test msie version and add class .gt-ieX x n
+ function addClassGtIe(){
+ if($.browser.msie){
+ var iVersion = parseInt($.browser.version),
+ iLowestVersion = 7; // version la plus basse prise en compte pour l'inté
+ for (i=iLowestVersion;i<iVersion+1;i++){// la version courante est supérieur aux versions précédentes
+ $('html').addClass('gt-ie'+(i-1));
+ }
+ }
+ }*/
+
+
+// Gestion du click sur le parent
+function addLinkBlock(){
+    $('.linkParent').each( function( ) {
+        var oElem = $(this).find('.linkBlock'),
+            sOnClick = oElem.attr('onclick');
+        if(sOnClick)
+            $(this).attr('onclick',sOnClick);
+        oElem.removeAttr('onclick');
+
+        $(this).css({cursor:'pointer'}).click(function(e) {
+            var event = e;
+            if (!e)
+                event = window.event;
+            if (event && event.target != oElem[0]) {
+                var sHref = oElem.attr('href'),
+                    sTarget = oElem.attr('target')?oElem.attr('target'):'_self';
+                //consoleLog(sHref);
+                switch (sTarget) {
+                    case "_blank":
+                        window.open(sHref, '');
+                        break;
+                    case "_parent":
+                        parent.location.href = sHref;
+                        break;
+                    case "_top":
+                        top.location.href = sHref;
+                        break;
+                    case "_self":
+                        document.location.href = sHref;
+                        break;
+                    default:
+                        sTarget.location.href = sHref;
+                        break;
+                }
+            }
+        });
+
+    });
+}
