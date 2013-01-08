c63fd1ebc4922913307b77bafe522c7a4a0c5979
Eric Fressange <e.fressangedubost@c2is.fr>
2013-01-08 12:28:18 +0100

    [HEADJS] ajout de points de rupture personnalises

--- html/includes/inc_header.php
+++ html/includes/inc_header.php
@@ -14,6 +14,29 @@
 
     <link rel="stylesheet" href="../css/screen.css">
 
+
+    <script>
+        // déclarations de points de rupture propre au site
+
+        var oBreakPoints = { width : [1350,1230,1024,800], height : [900,800,700]},
+            head_conf = {
+                width: oBreakPoints.width,
+                widthCss  : { "gt": true, "gte": true, "lt": true, "lte": true, "eq": true },
+                height: oBreakPoints.height,
+                heightCss : { "gt": true, "gte": true, "lt": true, "lte": true, "eq": true },
+                browsers  : [
+                    { ie     : { min: 6, max:  10 } }
+                    //,{ chrome : { min: 8, max: 24 } }
+                    //,{ ff     : { min: 3, max: 19 } }
+                    //,{ ios    : { min: 3, max:  6 } }
+                    //,{ android: { min: 2, max:  4 } }
+                    //,{ webkit : { min: 9, max: 12 } }
+                    //,{ opera  : { min: 9, max: 12 } }
+                ],
+                browserCss: { "gt": true, "gte": true, "lt": true, "lte": true, "eq": true }
+            };
+    </script>
+
     <script src="../js/libs/head.extended.js"></script><!--Version etendue de head.js avec + d options : syntaxe legerement differente -->
 
     <script>
